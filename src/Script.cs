2c462767e640fb58633c7c3212ce498a6dab0868
everlaster <everlaster@protonmail.com>
2022-07-13 22:09:40 +0300

    add .editorconfig, code cleanup

--- src/Script.cs
+++ src/Script.cs
@@ -2,203 +2,245 @@ using System;
 using UnityEngine;
 using System.Collections.Generic;
 
-// This class will produce a random number that can be used to set any float param available in all atoms
-// includes random generation period, smoothing, and range selection options
-public class Script : MVRScript {
-
-    protected void SyncAtomChoices() {
-        List<string> atomChoices = new List<string>();
+/// <summary>
+/// FloatParamRandomizerEE
+/// Extended edition of FloatParamRandomizer by MeshedVR.
+/// Randomizes float values with a smooth transition from current value towards target value.
+/// Source: https://github.com/everlasterVR/FloatParamRandomizerEE
+/// </summary>
+public class Script : MVRScript
+{
+    private void SyncAtomChoices()
+    {
+        var atomChoices = new List<string>();
         atomChoices.Add("None");
-        foreach (string atomUID in SuperController.singleton.GetAtomUIDs()) {
+        foreach(string atomUID in SuperController.singleton.GetAtomUIDs())
+        {
             atomChoices.Add(atomUID);
         }
-        atomJSON.choices = atomChoices;
+
+        _atomJSON.choices = atomChoices;
     }
 
-    // receiver Atom
-    protected Atom receivingAtom;
-    protected void SyncAtom(string atomUID) {
-        List<string> receiverChoices = new List<string>();
+    private Atom _receivingAtom;
+
+    private void SyncAtom(string atomUID)
+    {
+        var receiverChoices = new List<string>();
         receiverChoices.Add("None");
-        if (atomUID != null) {
-            receivingAtom = SuperController.singleton.GetAtomByUid(atomUID);
-            if (receivingAtom != null) {
-                foreach (string receiverChoice in receivingAtom.GetStorableIDs()) {
+        if(atomUID != null)
+        {
+            _receivingAtom = SuperController.singleton.GetAtomByUid(atomUID);
+            if(_receivingAtom != null)
+            {
+                foreach(string receiverChoice in _receivingAtom.GetStorableIDs())
+                {
                     receiverChoices.Add(receiverChoice);
                     //SuperController.LogMessage("Found receiver " + receiverChoice);
                 }
             }
-        } else {
-            receivingAtom = null;
         }
-        receiverJSON.choices = receiverChoices;
-        receiverJSON.val = "None";
-    }
-    protected JSONStorableStringChooser atomJSON;
+        else
+        {
+            _receivingAtom = null;
+        }
 
-    protected string _missingReceiverStoreId = "";
-    protected void CheckMissingReceiver() {
-        if (_missingReceiverStoreId != "" && receivingAtom != null) {
-            JSONStorable missingReceiver = receivingAtom.GetStorableByID(_missingReceiverStoreId);
-            if (missingReceiver != null) {
+        _receiverJSON.choices = receiverChoices;
+        _receiverJSON.val = "None";
+    }
+
+    private JSONStorableStringChooser _atomJSON;
+
+    private string _missingReceiverStoreId = "";
+
+    private void CheckMissingReceiver()
+    {
+        if(_missingReceiverStoreId != "" && _receivingAtom != null)
+        {
+            var missingReceiver = _receivingAtom.GetStorableByID(_missingReceiverStoreId);
+            if(missingReceiver != null)
+            {
                 //Debug.Log("Found late-loading receiver " + _missingReceiverStoreId);
                 string saveTargetName = _receiverTargetName;
                 SyncReceiver(_missingReceiverStoreId);
                 _missingReceiverStoreId = "";
                 insideRestore = true;
-                receiverTargetJSON.val = saveTargetName;
+                _receiverTargetJSON.val = saveTargetName;
                 insideRestore = false;
             }
         }
     }
 
-    // receiver JSONStorable
-    protected JSONStorable receiver;
-    protected void SyncReceiver(string receiverID) {
-        List<string> receiverTargetChoices = new List<string>();
+    private JSONStorable _receiver;
+
+    private void SyncReceiver(string receiverID)
+    {
+        var receiverTargetChoices = new List<string>();
         receiverTargetChoices.Add("None");
-        if (receivingAtom != null && receiverID != null) {
-            receiver = receivingAtom.GetStorableByID(receiverID);
-            if (receiver != null) {
-                foreach (string floatParam in receiver.GetFloatParamNames()) {
+        if(_receivingAtom != null && receiverID != null)
+        {
+            _receiver = _receivingAtom.GetStorableByID(receiverID);
+            if(_receiver != null)
+            {
+                foreach(string floatParam in _receiver.GetFloatParamNames())
+                {
                     receiverTargetChoices.Add(floatParam);
                 }
-            } else if (receiverID != "None") {
+            }
+            else if(receiverID != "None")
+            {
                 // some storables can be late loaded, like skin, clothing, hair, etc so must keep track of missing receiver
                 //Debug.Log("Missing receiver " + receiverID);
                 _missingReceiverStoreId = receiverID;
             }
-        } else {
-            receiver = null;
         }
-        receiverTargetJSON.choices = receiverTargetChoices;
-        receiverTargetJSON.val = "None";
-    }
-    protected JSONStorableStringChooser receiverJSON;
+        else
+        {
+            _receiver = null;
+        }
 
-    // receiver target parameter
-    protected string _receiverTargetName;
-    protected JSONStorableFloat receiverTarget;
-    protected void SyncReceiverTarget(string receiverTargetName) {
+        _receiverTargetJSON.choices = receiverTargetChoices;
+        _receiverTargetJSON.val = "None";
+    }
+
+    private JSONStorableStringChooser _receiverJSON;
+
+    private string _receiverTargetName;
+    private JSONStorableFloat _receiverTarget;
+
+    private void SyncReceiverTarget(string receiverTargetName)
+    {
         _receiverTargetName = receiverTargetName;
-        receiverTarget = null;
-        if (receiver != null && receiverTargetName != null) {
-            receiverTarget = receiver.GetFloatJSONParam(receiverTargetName);
-            if (receiverTarget != null) {
-                lowerValueJSON.min = receiverTarget.min;
-                lowerValueJSON.max = receiverTarget.max;
-                upperValueJSON.min = receiverTarget.min;
-                upperValueJSON.max = receiverTarget.max;
-                currentValueJSON.min = receiverTarget.min;
-                currentValueJSON.max = receiverTarget.max;
-                targetValueJSON.min = receiverTarget.min;
-                targetValueJSON.max = receiverTarget.max;
-                if (!insideRestore) {
+        _receiverTarget = null;
+        if(_receiver != null && receiverTargetName != null)
+        {
+            _receiverTarget = _receiver.GetFloatJSONParam(receiverTargetName);
+            if(_receiverTarget != null)
+            {
+                _lowerValueJSON.min = _receiverTarget.min;
+                _lowerValueJSON.max = _receiverTarget.max;
+                _upperValueJSON.min = _receiverTarget.min;
+                _upperValueJSON.max = _receiverTarget.max;
+                _currentValueJSON.min = _receiverTarget.min;
+                _currentValueJSON.max = _receiverTarget.max;
+                _targetValueJSON.min = _receiverTarget.min;
+                _targetValueJSON.max = _receiverTarget.max;
+                if(!insideRestore)
+                {
                     // only sync up val if not in restore
-                    lowerValueJSON.val = receiverTarget.val;
-                    upperValueJSON.val = receiverTarget.val;
-                    currentValueJSON.val = receiverTarget.val;
-                    targetValueJSON.val = receiverTarget.val;
+                    _lowerValueJSON.val = _receiverTarget.val;
+                    _upperValueJSON.val = _receiverTarget.val;
+                    _currentValueJSON.val = _receiverTarget.val;
+                    _targetValueJSON.val = _receiverTarget.val;
                 }
             }
         }
     }
-    protected JSONStorableStringChooser receiverTargetJSON;
 
-    protected JSONStorableFloat periodJSON;
-    protected JSONStorableFloat quicknessJSON;
-    protected JSONStorableFloat lowerValueJSON;
-    protected JSONStorableFloat upperValueJSON;
-    protected JSONStorableFloat targetValueJSON;
-    protected JSONStorableFloat currentValueJSON;
+    private JSONStorableStringChooser _receiverTargetJSON;
 
-    public override void Init() {
-        try {
+    private JSONStorableFloat _periodJSON;
+    private JSONStorableFloat _quicknessJSON;
+    private JSONStorableFloat _lowerValueJSON;
+    private JSONStorableFloat _upperValueJSON;
+    private JSONStorableFloat _targetValueJSON;
+    private JSONStorableFloat _currentValueJSON;
+
+    public override void Init()
+    {
+        try
+        {
             // make atom selector
-            atomJSON = new JSONStorableStringChooser("atom", SuperController.singleton.GetAtomUIDs(), null, "Atom", SyncAtom);
-            atomJSON.representsAtomUid = true;
-            RegisterStringChooser(atomJSON);
+            _atomJSON = new JSONStorableStringChooser("atom", SuperController.singleton.GetAtomUIDs(), null, "Atom", SyncAtom);
+            _atomJSON.representsAtomUid = true;
+            RegisterStringChooser(_atomJSON);
             SyncAtomChoices();
-            UIDynamicPopup dp = CreateFilterablePopup(atomJSON);
+            var dp = CreateFilterablePopup(_atomJSON);
             dp.popupPanelHeight = 1100f;
             // want to always resync the atom choices on opening popup since atoms can be added/removed
             dp.popup.onOpenPopupHandlers += SyncAtomChoices;
 
             // make receiver selector
-            receiverJSON = new JSONStorableStringChooser("receiver", null, null, "Receiver", SyncReceiver);
-            RegisterStringChooser(receiverJSON);
-            dp = CreateFilterablePopup(receiverJSON);
+            _receiverJSON = new JSONStorableStringChooser("receiver", null, null, "Receiver", SyncReceiver);
+            RegisterStringChooser(_receiverJSON);
+            dp = CreateFilterablePopup(_receiverJSON);
             dp.popupPanelHeight = 960f;
 
             // make receiver target selector
-            receiverTargetJSON = new JSONStorableStringChooser("receiverTarget", null, null, "Target", SyncReceiverTarget);
-            RegisterStringChooser(receiverTargetJSON);
-            dp = CreateFilterablePopup(receiverTargetJSON);
+            _receiverTargetJSON = new JSONStorableStringChooser("receiverTarget", null, null, "Target", SyncReceiverTarget);
+            RegisterStringChooser(_receiverTargetJSON);
+            dp = CreateFilterablePopup(_receiverTargetJSON);
             dp.popupPanelHeight = 820f;
 
             // set atom to current atom to initialize
-            atomJSON.val = containingAtom.uid;
+            _atomJSON.val = containingAtom.uid;
 
             // create random value generation period
-            periodJSON = new JSONStorableFloat("period", 1f, 0f, 10f, false);
-            RegisterFloat(periodJSON);
-            CreateSlider(periodJSON, true);
+            _periodJSON = new JSONStorableFloat("period", 1f, 0f, 10f, false);
+            RegisterFloat(_periodJSON);
+            CreateSlider(_periodJSON, true);
 
             // quickness (smoothness)
-            quicknessJSON = new JSONStorableFloat("quickness", 1f, 0f, 10f, true);
-            RegisterFloat(quicknessJSON);
-            CreateSlider(quicknessJSON, true);
+            _quicknessJSON = new JSONStorableFloat("quickness", 1f, 0f, 10f);
+            RegisterFloat(_quicknessJSON);
+            CreateSlider(_quicknessJSON, true);
 
             // lower val
-            lowerValueJSON = new JSONStorableFloat("lowerValue", 0f, 0f, 1f, false);
-            RegisterFloat(lowerValueJSON);
-            CreateSlider(lowerValueJSON, true);
+            _lowerValueJSON = new JSONStorableFloat("lowerValue", 0f, 0f, 1f, false);
+            RegisterFloat(_lowerValueJSON);
+            CreateSlider(_lowerValueJSON, true);
 
             // upper val
-            upperValueJSON = new JSONStorableFloat("upperValue", 0f, 0f, 1f, false);
-            RegisterFloat(upperValueJSON);
-            CreateSlider(upperValueJSON, true);
+            _upperValueJSON = new JSONStorableFloat("upperValue", 0f, 0f, 1f, false);
+            RegisterFloat(_upperValueJSON);
+            CreateSlider(_upperValueJSON, true);
 
             // target val
-            targetValueJSON = new JSONStorableFloat("targetValue", 0f, 0f, 1f, false, false);
+            _targetValueJSON = new JSONStorableFloat("targetValue", 0f, 0f, 1f, false, false);
             // don't register - this is for viewing only and is generated
-            UIDynamicSlider ds = CreateSlider(targetValueJSON, true);
+            var ds = CreateSlider(_targetValueJSON, true);
             ds.defaultButtonEnabled = false;
             ds.quickButtonsEnabled = false;
 
             // current val
-            currentValueJSON = new JSONStorableFloat("currentValue", 0f, 0f, 1f, false, false);
+            _currentValueJSON = new JSONStorableFloat("currentValue", 0f, 0f, 1f, false, false);
             // don't register - this is for viewing only and is generated
-            ds = CreateSlider(currentValueJSON, true);
+            ds = CreateSlider(_currentValueJSON, true);
             ds.defaultButtonEnabled = false;
             ds.quickButtonsEnabled = false;
 
         }
-        catch (Exception e) {
+        catch(Exception e)
+        {
             SuperController.LogError("Exception caught: " + e);
         }
     }
 
-    protected float accumulated = 0f;
-    protected float start = 0f;
+    private float _accumulated;
+    private float _start;
 
-    protected void Update() {
-        try {
-            if (accumulated > periodJSON.val) {
-                accumulated = 0f;
-                start = currentValueJSON.val;
-                targetValueJSON.val = UnityEngine.Random.Range(lowerValueJSON.val, upperValueJSON.val);
+    protected void Update()
+    {
+        try
+        {
+            if(_accumulated > _periodJSON.val)
+            {
+                _accumulated = 0f;
+                _start = _currentValueJSON.val;
+                _targetValueJSON.val = UnityEngine.Random.Range(_lowerValueJSON.val, _upperValueJSON.val);
             }
 
-            accumulated += Time.deltaTime;
-            currentValueJSON.val = Mathf.SmoothStep(start, targetValueJSON.val, accumulated * quicknessJSON.val / periodJSON.val);
+            _accumulated += Time.deltaTime;
+            _currentValueJSON.val = Mathf.SmoothStep(_start, _targetValueJSON.val, _accumulated * _quicknessJSON.val / _periodJSON.val);
 
             CheckMissingReceiver();
-            if (receiverTarget != null) {
-                receiverTarget.val = currentValueJSON.val;
+            if(_receiverTarget != null)
+            {
+                _receiverTarget.val = _currentValueJSON.val;
             }
         }
-        catch (Exception e) {
+        catch(Exception e)
+        {
             SuperController.LogError("Exception caught: " + e);
         }
     }
